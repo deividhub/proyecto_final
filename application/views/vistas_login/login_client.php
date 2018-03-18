06cf1ed7d323da424e7a2fcf57383ded07b7296d
nereafptxurdinaga <31986450+nereafptxurdinaga@users.noreply.github.com>
2018-03-18 12:21:50 +0100

    login-registro juntos

--- application/views/vistas_login/login_client.php
+++ application/views/vistas_login/login_client.php
@@ -1,20 +1,52 @@
 <body id="login_client">
 
-	<section>
-
-		<h1>Inicio de sesión</h1>
+	<section id="juntos">
 		
-		<hr>
+		<h1 id="titulo">¡Accede a nuestra tienda!</h1>
 
-		<form>
-			<input type="email" class="correo" placeholder="ejemplo@ejemplo.com">
-			<input type="password" placeholder="Contraseña">
-			<input type="submit" value="Acceder">			
-		</form>
+		<section id="login">
+
+			<h1>Inicio de sesión</h1>
+			
+			<hr>
+
+			<form>
+				<input type="email" class="correo" placeholder="ejemplo@ejemplo.com">
+				<input type="password" placeholder="Contraseña">
+				<input type="submit" value="Acceder">			
+			</form>
+			
+			<hr>
+
+			<p class="developed">DWNPD</p>
+
+		</section>
 		
-		<hr>
 
-		<p class="developed">DWNPD</p>
+		<section id="registro">
+
+			<h1>Registro</h1>
+			
+			<hr>
+
+			<form>
+				
+				<input type="text" placeholder="Nombre">
+				<input type="text" placeholder="Apellidos">
+				<input type="date" placeholder="DD/MM/YYYY">
+				<input type="text" placeholder="Domicilio">
+				<input type="text" placeholder="Provincia">
+				<input type="text" placeholder="Localidad">
+				<input type="email" class="correo" placeholder="ejemplo@ejemplo.com">
+				<input type="password" placeholder="Contraseña">
+				<input type="submit" value="Registrarse">			
+			</form>
+			
+			<hr>
+
+			<p class="developed">DWNPD</p>
+
+		</section>
 
 	</section>
 
