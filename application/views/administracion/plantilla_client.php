567e1bfca476610fc1a30bb92d4d5495c2b5d52e
nereafptxurdinaga <31986450+nereafptxurdinaga@users.noreply.github.com>
2018-03-18 12:01:15 +0100

    vistas

---
+++ application/views/administracion/plantilla_client.php
@@ -0,0 +1,87 @@
+<body id="administrar_cliente">
+
+	<header>
+
+		<ul>
+		    <li>Perfíl personal</li>
+		    <li><i class="material-icons rounded">person</i></li>
+		    <li><a href="<?php echo base_url();?>index.php/Principal">Volver a la tienda</a></li>
+		</ul>
+
+	</header>
+
+
+	<section>
+
+		<aside>
+
+			<ul>
+			    <li><a href="">Datos personales</a></li>
+			    <li><a href="">Pedidos</a></li>
+			    <li><a href="">Notificar errores</a></li>
+			</ul>
+
+		</aside>
+
+		<article id="datos_personales">
+
+			<h1>Gestión de datos personales</h1>
+
+			<form class="form_client">
+			
+				<input type="text" id="form_nombre_cliente">
+
+				<input type="text" id="form_apellidos_cliente">	
+
+				<input type="date" id="form_fecha_cliente">
+
+				<input type="text" id="form_domicilio_cliente">
+
+				<input type="text" id="form_provincia_cliente">
+
+				<input type="text" id="form_localidad_cliente">
+
+				<input type="email" id="form_correo_cliente">
+
+			</form>
+
+			<p class="developed">Desarrollado por: Nerea - Pedro - David</p>
+
+		</article>
+	
+		<!--
+		<article id="pedidos">
+
+			<h1>Gestión de pedidos</h1>
+
+			<form class="form_client">
+			
+				<input type="text" id="form_nombre_cliente">
+
+				<input type="text" id="form_apellidos_cliente">	
+
+				<input type="date" id="form_fecha_cliente">
+
+				<input type="text" id="form_domicilio_cliente">
+
+				<input type="text" id="form_provincia_cliente">
+
+				<input type="text" id="form_localidad_cliente">
+
+				<input type="email" id="form_correo_cliente">
+
+			</form>
+
+			<p class="developed">Desarrollado por: Nerea - Pedro - David</p>
+			
+		</article>
+		-->
+	</section>
+
+</body>
+</html>
+
+
+
+
+
